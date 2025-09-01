bb3c186b786017468a048368a2e1a6b77654fc96
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-01 06:17:16 +0300

    chore: initialize project

---
+++ src/main/java/colombe/simpletodolist/SimpleTodoListApplication.java
@@ -0,0 +1,12 @@
+package colombe.simpletodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleTodoListApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SimpleTodoListApplication.class, args);
+  }
+}
---
+++ src/test/java/colombe/simpletodolist/SimpleTodoListApplicationTests.java
@@ -0,0 +1,11 @@
+package colombe.simpletodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleTodoListApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
