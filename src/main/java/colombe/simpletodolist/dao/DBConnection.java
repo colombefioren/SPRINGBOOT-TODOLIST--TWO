4e3a4a9dcbfbb618cae88190764b554dbfa2a509
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-01 06:38:54 +0300

    feat: reutilisable connection utility

---
+++ src/main/java/colombe/simpletodolist/dao/DBConnection.java
@@ -0,0 +1,15 @@
+package colombe.simpletodolist.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+  public static final String DB_URL = "jdbc:mysql://localhost:5433/springboot_todolist_two";
+  public static final String DB_USER = "postgres";
+  public static final String DB_PASSWORD = "1234";
+
+  public static Connection getConnection() throws SQLException {
+    return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+  }
+}
