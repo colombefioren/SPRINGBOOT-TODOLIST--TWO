3ad4f5acd2e67d9f94d37bbd679f3e8410da3214
colombefioren <colomberakotonjanahary@gmail.com>
2025-09-01 07:36:02 +0300

    feat: todo controller

---
+++ src/main/java/colombe/simpletodolist/controller/TodoController.java
@@ -0,0 +1,34 @@
+package colombe.simpletodolist.controller;
+
+import colombe.simpletodolist.entity.Todo;
+import colombe.simpletodolist.service.TodoService;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@AllArgsConstructor
+@RestController
+@RequestMapping("/todos")
+public class TodoController {
+  private final TodoService todoService = new TodoService();
+
+  @GetMapping
+  public List<Todo> getAllTodo() {
+    return todoService.getAllTodo();
+  }
+
+  @GetMapping("/{id}")
+  public Todo getTodoById(@PathVariable int id) {
+    return todoService.getTodoById(id);
+  }
+
+  @GetMapping
+  public void createTodo(@RequestBody Todo todo) {
+    todoService.createTodo(todo);
+  }
+
+  @GetMapping("/{id}")
+  public void deleteTodo(@PathVariable int id) {
+    todoService.deleteTodo(id);
+  }
+}
--- src/main/java/colombe/simpletodolist/service/TodoService.java
+++ src/main/java/colombe/simpletodolist/service/TodoService.java
@@ -3,29 +3,32 @@ package colombe.simpletodolist.service;
 import colombe.simpletodolist.dao.TodoDAOImp;
 import colombe.simpletodolist.entity.Todo;
 import java.util.List;
-import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-@AllArgsConstructor
+@Service
 public class TodoService {
   private final TodoDAOImp TodoDAO;
 
-  public List<Todo> getAllTodo(){
-      return TodoDAO.getAllTodo();
+  public TodoService() {
+    this.TodoDAO = new TodoDAOImp();
   }
 
-  public Todo getTodoById(int id){
-      return TodoDAO.getTodoById(id);
+  public List<Todo> getAllTodo() {
+    return TodoDAO.getAllTodo();
   }
 
-  public void createTodo(Todo todo){
-      if(todo.getTitle() == null || todo.getTitle().isEmpty()){
-          throw new IllegalArgumentException("Title cannot be empty");
-      }
-      TodoDAO.createTodo(todo);
+  public Todo getTodoById(int id) {
+    return TodoDAO.getTodoById(id);
   }
 
-  public void deleteTodo(int id){
-      TodoDAO.deleteTodo(id);
+  public void createTodo(Todo todo) {
+    if (todo.getTitle() == null || todo.getTitle().isEmpty()) {
+      throw new IllegalArgumentException("Title cannot be empty");
+    }
+    TodoDAO.createTodo(todo);
   }
 
+  public void deleteTodo(int id) {
+    TodoDAO.deleteTodo(id);
+  }
 }
